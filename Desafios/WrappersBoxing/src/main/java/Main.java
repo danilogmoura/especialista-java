b1aa3f13c11d6fac54dfba8b0388077ec6bf256d
Danilo <danilogmoura@gmail.com>
2024-02-27 21:24:39 -0300

    Desafio, wrappers e boxing

---
+++ Desafios/WrappersBoxing/src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Televisor tv = new Televisor();
+
+        // Não deveria mudar o volume e canal
+        tv.mudarCanal(130);
+        tv.mudarVolume(20);
+
+
+        // Deveria mudar o volume e canal
+        tv.mudarCanal(10);
+        tv.mudarVolume(300);
+    }
+
+}
