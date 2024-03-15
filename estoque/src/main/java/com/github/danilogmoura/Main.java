851c44eedd751922a7d62e3698ea9bfb6e323b5b
Danilo <danilogmoura@gmail.com>
2024-03-15 16:37:34 -0300

    16.6. Capturando exceções com múltiplos blocos catch

--- estoque/src/main/java/com/github/danilogmoura/Main.java
+++ estoque/src/main/java/com/github/danilogmoura/Main.java
@@ -2,15 +2,55 @@ package com.github.danilogmoura;
 
 import com.github.danilogmoura.estoque.Produto;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         var produto = new Produto("Apple Watch");
-        produto.adicionaEstoque(10);
-        produto.ativar();
+//        produto.ativar();
+        produto.adicionaEstoque(20);
 
-        produto.retirarQuantidade(15);
+        comprar(produto);
+    }
 
-        System.out.printf("Estoque %d%n", produto.getQuantidadeEstoque());
+    private static void comprar(Produto produto) {
+        var scanner = new Scanner(System.in);
+
+        do {
+            try {
+                System.out.print("Quantidade: ");
+                var quantidade = scanner.nextInt();
+
+                efetuarBaixaEstoque(produto, quantidade);
+                System.out.println("Compra realizada");
+
+                break;
+            } catch (IllegalArgumentException iae) {
+                iae.printStackTrace();
+                System.out.println("Erro na compra: " + iae.getMessage());
+            } catch (IllegalStateException ise) {
+                System.out.println("Erro na compra: " + ise.getMessage());
+                System.out.print("Deseja ativar o produto? ");
+                if (scanner.nextBoolean()) {
+                    produto.ativar();
+                    System.out.println("Ok. Produto ativado");
+                } else {
+                    System.out.println("Ok. Compra não pode ser realizada");
+                    break;
+                }
+            }
+        } while (true);
+    }
+
+    private static void efetuarBaixaEstoque(Produto produto, int quantidade) {
+//        try {
+        produto.retirarQuantidade(quantidade);
+        System.out.printf("%d unidades retiradas do estoque. Estoque atual: %d%n",
+                quantidade, produto.getQuantidadeEstoque());
+//        } catch (IllegalArgumentException iae) {
+//            System.out.println("Erro ao efetuar baixa no extoque: " + iae.getMessage());
+//            throw iae;
+//        }
     }
 }
